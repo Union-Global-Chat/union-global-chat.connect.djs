6dad04619498a1f8ba49a8d4e6118430bf8e07af
れのらり <renorari@renorari.net>
2022-08-11 17:59:46 +0900

    いい感じにした

--- index.js
+++ index.js
@@ -3,5 +3,14 @@
 /*          (c) 2022 Renorari           */
 
 module.exports = {
+    /**
+     * Union Global Chat Client
+     * @constructor
+     * @param {string} token - Union Global Chat Token
+     * @fires Client#close
+     * @fires Client#error
+     * @fires Client#message
+     * @fires Client#ready
+     */
     Client: require("./src/Client")
 };
